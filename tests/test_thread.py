6559d3b645c1891d44604304f8b6c6be00f44777
Xiao tian Liu <v-xiaotiliu@microsoft.com>
2024-09-04 16:39:27 +0800

    [Refine] my_thread

--- tests/test_thread.py
+++ tests/test_thread.py
@@ -1,7 +1,8 @@
 import os, sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import pytest, logging
+import pytest, logging, asyncio
+from time import time
 from instances.my_thread import ThreadManager, ExampleThreadManager
 
 
@@ -13,7 +14,10 @@ async def fibonacci_async(n):
     elif n == 2:
         return 1
     else:
-        await fibonacci_async(n-1) + fibonacci_async(n-2)
+        # 并发执行两个异步递归调用
+        result_0 = await fibonacci_async(n-1)
+        result_1 = await fibonacci_async(n-2)
+        return result_0 + result_1
 
 def fibonacci(n):
     if n <= 0:
@@ -27,17 +31,20 @@ def fibonacci(n):
 
 def test_thread_manager():
     # We instantiate the ThreadManager
-    manager = ExampleThreadManager(fibonacci, thread_num=50, 
+    manager = ExampleThreadManager(fibonacci, thread_num=6, 
                                    show_progress=True)
 
     # Assuming dictory is the list of tasks you want to test
-    results = manager.test_methods([30]*100)
+    results = manager.test_methods([30]*12)
     logging.info(results)
 
-# def test_multiprocessing():
-#     manager = ExampleThreadManager(generate_new_tasks, thread_num=6, 
-#                                    show_progress=False)
+@pytest.mark.asyncio
+async def test_thread_manager_async():
+    # We instantiate the ThreadManager
+    manager = ExampleThreadManager(fibonacci_async, thread_num=6,
+                                   show_progress=True)
 
-#     start_time = time()
-#     manager.start(range(100), 'multiprocessing')
-#     logging.info(time() - start_time)
+    # Assuming dictory is the list of tasks you want to test
+    start = time()
+    await manager.start_async([30]*12)
+    logging.info(f'run_in_serial: {time() - start}')
